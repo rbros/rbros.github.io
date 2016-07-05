e6a62a844607128c6ed1f6e85a359ddb29a46cca
Cody Reichert <cody@reichertbrothers.com>
2016-07-05 18:13:25 -0500

    Update deploy script for new repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,25 +13,23 @@ rev=$(git rev-parse --short HEAD)
 
 # Make a directory where we can build the files We will CD into this
 # directory, create a git repo, copy files from parent directory, and
-# then push this repo to reichertbrothers.com gh-pages branch.
+# then push this repo to rbros.github.io master branch.
 mkdir stage
 cd stage
 
-# Set git settings
+# Git settings
 git init
 git config user.name "Cody Reichert"
 git config user.email "cody@reichertbrothers.com"
 
-# Check out gh-pages branch of reichertbrothers.com repo
-# Note: GH_TOKEN was entered into the Travis-CI environment manually
-# through the UI.
-git remote add upstream "https://$GH_TOKEN@github.com/rbros/reichertbrothers.com.git"
+# Check out master branch of rbros.github.io repo Note: GH_TOKEN was
+# entered into the Travis-CI environment manually through the UI.
+git remote add upstream "https://$GH_TOKEN@github.com/rbros/rbros.github.io.git"
 git fetch upstream
-git reset upstream/gh-pages
+git reset upstream/master
 
-# touch .
 cp -R $TRAVIS_BUILD_DIR/_site/* .
 
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git push -q upstream HEAD:master
